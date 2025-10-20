9700468de28345908ba1068d6454ed924188351f
Kitthapat j <admin01@VM000.com>
2025-10-20 22:56:57 +0000

    update commit: Azure Resource Group and Insecure NSG

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ resource "azurerm_resource_group" "rg" {
 
 # 2. Azure Storage Account (Vulnerable by Design: Public Access is ON)
 resource "azurerm_storage_account" "storage" {
-  name                     = "secopslabstorage002" # ต้องไม่ซ้ำใครทั่วโลก!
+  name                     = "secopslabstorage0324341102" # ต้องไม่ซ้ำใครทั่วโลก!
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   
   # ช่องโหว่: อนุญาตการเข้าถึง Public Blob - Terrascan ต้องจับได้!
-  allow_blob_public_access = true 
+ allow_nested_items_to_be_public = true
 }
